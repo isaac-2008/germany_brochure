21428290bd3639fb731a8d18ed234274be19b1fe
IsaacFS <isaacfornerosaatt2008@gmail.com>
2026-08-13 13:38:04 -0300

    Refactor animation logic in GermanyFlag component

--- GermanyFlag.js
+++ GermanyFlag.js
@@ -1,25 +1,36 @@
 import React, { useEffect, useRef } from "react";
-import { Animated, View, StyleSheet } from "react-native";
+import {
+  Animated,
+  View,
+  StyleSheet,
+} from "react-native";
 
 export default function GermanyFlag() {
   const wave = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    Animated.loop(
+    const animation = Animated.loop(
       Animated.sequence([
         Animated.timing(wave, {
           toValue: 1,
           duration: 1000,
           useNativeDriver: true,
         }),
+
         Animated.timing(wave, {
           toValue: 0,
           duration: 1000,
           useNativeDriver: true,
         }),
       ])
-    ).start();
-  }, []);
+    );
+
+    animation.start();
+
+    return () => {
+      animation.stop();
+    };
+  }, [wave]);
 
   const rotate = wave.interpolate({
     inputRange: [0, 1],
@@ -35,9 +46,32 @@ export default function GermanyFlag() {
         },
       ]}
     >
-      <View style={[styles.band, { backgroundColor: "#000" }]} />
-      <View style={[styles.band, { backgroundColor: "#DD0000" }]} />
-      <View style={[styles.band, { backgroundColor: "#FFCE00" }]} />
+      <View
+        style={[
+          styles.band,
+          {
+            backgroundColor: "#000000",
+          },
+        ]}
+      />
+
+      <View
+        style={[
+          styles.band,
+          {
+            backgroundColor: "#DD0000",
+          },
+        ]}
+      />
+
+      <View
+        style={[
+          styles.band,
+          {
+            backgroundColor: "#FFCE00",
+          },
+        ]}
+      />
     </Animated.View>
   );
 }
@@ -48,6 +82,7 @@ const styles = StyleSheet.create({
     height: 160,
     borderRadius: 15,
     overflow: "hidden",
+    alignSelf: "center",
   },
 
   band: {
