6107b10f3b0e01fd0d88b49f98a4f827fe7c61b8
IsaacFS <isaacfornerosaatt2008@gmail.com>
2026-08-13 13:34:46 -0300

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,22 +1,40 @@
-import { View, Text } from "react-native";
+import React from "react";
+
+import {
+  View,
+  Text,
+  StyleSheet,
+} from "react-native";
 
 export default function Footer() {
   return (
-    <View
-      style={{
-        marginVertical: 30,
-        alignItems: "center",
-      }}
-    >
-      <Text
-        style={{
-          color: "#FFCE00",
-          fontWeight: "bold",
-          fontSize: 18,
-        }}
-      >
+    <View style={styles.container}>
+      <Text style={styles.text}>
         Discover Germany 🇩🇪
+      </Text>
+
+      <Text style={styles.subtext}>
+        Travel • Explore • Enjoy
       </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 30,
+    alignItems: "center",
+  },
+
+  text: {
+    color: "#FFCE00",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+
+  subtext: {
+    color: "#FFFFFF",
+    fontSize: 13,
+    marginTop: 5,
+  },
+});
